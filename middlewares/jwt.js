9ceb3924323e0fa59f6af291902bd52e8cc0bd9c
Dhananjay <dhananjaykatare24@gmail.com>
2025-08-30 22:06:27 +0530

    added JWT

---
+++ middlewares/jwt.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler")
+
+const jwtMiddleware = asyncHandler(async (req, res) => {
+    let token;
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    
+    if(authHeader && authHeader.startsWith("bearer")){
+        token = authHeader.split(" ")[1];
+
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded)=>{
+            if(err){
+                res.status(401);
+                throw new Error("User is not authorized!");
+            }
+            console.log(decoded);
+        });
+    }
+});
+
+const generateToken = asyncHandler(async (userData) => {
+    jwt.sign(userData, process.env.JWT_SECRET_KEY, {expiresIn: "1m"})
+});
+
+module.exports = {jwtMiddleware, generateToken};
